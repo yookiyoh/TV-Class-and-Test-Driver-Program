cd52b5cf78838d23be3f29c86fc1e127caee4936
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-05-13 16:57:25 +0800

    initial pseudocode flow

--- OOP TV Class and Test Driver.py
+++ OOP TV Class and Test Driver.py
@@ -2,3 +2,13 @@
 # BSCpE 1-5
 # Object-Oriented Programming | Assignment 6
 # OOP TV Class and Test Driver Program
+
+# Pseudocode
+
+# create a class for TV
+# enable TV class constructor/s
+# execute TV class methods
+# define TestTV class
+# have TestTV constructor/s
+# enable methods for TestTV class
+# create instance of TestTV
